8603044ebe93b4eaf5abd2b50aabf3eda71a2672
arjunrathore05 <arjunrathore1462005xgr2@gmail.com>
2026-06-01 21:20:30 +0530

    Create Remove_Duplicates_From_Sorted_Array.cpp

---
+++ Week_1/Day_1/Remove_Duplicates_From_Sorted_Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+   
+    int removeDuplicates(vector<int>& nums) {
+        int idx = 0;   
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                nums[idx] = nums[i];
+                idx++;
+            }
+        }
+        return idx;
+    }
+};
