56a5a3bcab70a0669fa1eb4e197e6411733ef078
arjunrathore05 <arjunrathore1462005xgr2@gmail.com>
2026-06-04 16:28:27 +0530

    Create Spiral_Matrix .cpp

---
+++ Week_1/Day_4/Spiral_Matrix .cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+        int rowstart = 0;
+        int colstart = 0;
+        int rowend = matrix.size() - 1;
+        int colend = matrix[0].size() - 1;
+
+        while (rowstart <= rowend && colstart <= colend) {
+
+            for (int i = colstart; i <= colend; i++) {
+                ans.push_back(matrix[rowstart][i]);
+            }
+            rowstart++;
+            for (int i = rowstart; i <= rowend; i++) {
+                ans.push_back(matrix[i][colend]);
+            }
+            colend--;
+            if (rowstart <= rowend) {
+                for (int i = colend; i >= colstart; i--) {
+                    ans.push_back(matrix[rowend][i]);
+                }
+                rowend--;
+            }
+            if (colstart <= colend) {
+                for (int i = rowend; i >= rowstart; i--) {
+                    ans.push_back(matrix[i][colstart]);
+                }
+                colstart++;
+            }
+        }
+        return ans;
+    }
+};
